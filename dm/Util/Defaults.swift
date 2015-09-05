5b4abdee6b63e0436a7a7bb92bdfbebfb004b8d7
mpc <mcampbell@elevenfiftyconsulting.com>
2015-09-05 16:04:39 -0400

    Add controls to the login screen Create a button class Set constraints on login screen

---
+++ dm/Util/Defaults.swift
@@ -0,0 +1,22 @@
+//
+//  Defaults.swift
+//  dm
+//
+//  Created by mpc on 9/5/15.
+//  Copyright (c) 2015 mpc. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class DMButton : UIButton {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 5.0;
+        self.layer.borderWidth  = 0.5
+        self.layer.borderColor  = UIColor.blackColor().CGColor
+        self.backgroundColor    = UIColor.whiteColor()
+        self.tintColor          = UIColor.whiteColor()
+        
+    }
+}
