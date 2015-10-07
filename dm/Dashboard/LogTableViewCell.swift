3961f5e9e4e70d8a4f44e8ff81d0fbbbae7938cd
mpc <mcampbell@elevenfiftyconsulting.com>
2015-10-06 23:16:05 -0400

    Add Log screen and controllers

---
+++ dm/Dashboard/LogTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LogTableViewCell.swift
+//  dm
+//
+//  Created by mpc on 10/6/15.
+//  Copyright © 2015 mpc. All rights reserved.
+//
+
+import UIKit
+
+class LogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
