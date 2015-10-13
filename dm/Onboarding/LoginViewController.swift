3cb1ca466d9dfb91c3376e834f862484857b0a30
mpc <mcampbell@elevenfiftyconsulting.com>
2015-10-13 19:10:22 -0400

    Additional constraints.  Have the basic widgets and views constrained correctly.  Need to handle other widgets.

--- dm/Onboarding/LoginViewController.swift
+++ dm/Onboarding/LoginViewController.swift
@@ -25,27 +25,103 @@ class LoginViewController: UIViewController {
     func setConstraints() {
         loginView.frame = CGRectMake(0, 0, super.view.frame.width, super.view.frame.height)
         
-        print("w = \(super.view.frame.width)  h = \(super.view.frame.height)")
+        let viewCenter   = loginView.frame.width / 2
+        let widgetCenter = username.frame.width / 2
+        
+        print("viewCenter = \(viewCenter) widgetCenter \(widgetCenter)")
         
         
-        loginView.snp_makeConstraints { (make) -> Void in
-            make.height.equalTo(super.view.frame.height)
-            make.width.equalTo(super.view.frame.width)
-        }
-        
+        //TODO: *** Center the view, not the widgets ***
         
         username.snp_makeConstraints { (make) -> Void in
             make.top.equalTo(loginView.snp_top).offset(51)
             make.width.equalTo(204)
             make.height.equalTo(28)
-            
-            let frameCenter   = Int(super.view.frame.width / 2.0)
-            let textBoxCenter = Int(username.frame.width / 2.0)
-            let centerVal     = CGFloat(frameCenter - textBoxCenter)
-
-            make.centerX.equalTo(centerVal)
+            make.left.equalTo(widgetCenter / 2)
         }
+        
+        password.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(username.snp_bottom).offset(3)
+            make.width.equalTo(204)
+            make.height.equalTo(28)
+            make.left.equalTo(widgetCenter / 2)
+        }
+        
+        loginButton.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(password.snp_bottom).offset(15)
+            make.width.equalTo(204)
+            make.height.equalTo(28)
+            make.left.equalTo(password.snp_left)
+        }
+        
+        bannerView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(loginButton.snp_bottom).offset(30)
+            make.width.equalTo(super.view.frame.width)
+            make.height.equalTo(54)
+        }
+        
     }
+    
+    
+    
+    
+    
+    
+//    func setConstraints() {
+//        loginView.frame = CGRectMake(0, 0, super.view.frame.width, super.view.frame.height)
+//        
+//        print("w = \(super.view.frame.width)  h = \(super.view.frame.height)")
+//        
+//        
+//        loginView.snp_remakeConstraints { (make) -> Void in
+//            make.height.equalTo(super.view.frame.height)
+//            make.width.equalTo(super.view.frame.width)
+//        }
+//
+//        username.snp_makeConstraints { (make) -> Void in
+//            make.top.equalTo(loginView.snp_top).offset(51)
+//            make.width.equalTo(204)
+//            make.height.equalTo(28)
+//            
+//            //TODO: Make a resubale method for this...center(a, b) -> cVal
+//            let frameCenter   = Int(super.view.frame.width / 2.0)
+//            let textBoxCenter = Int(username.frame.width / 2.0)
+//            let centerVal     = CGFloat(frameCenter - textBoxCenter)
+//
+//            print("fc = \(frameCenter)  tbc = \(textBoxCenter)  cv = \(centerVal)")
+//            
+//            make.left.equalTo(centerVal)
+//        }
+//        
+//        password.snp_makeConstraints { (make) -> Void in
+//            make.top.equalTo(username.snp_bottom).offset(6)
+//            make.width.equalTo(204)
+//            make.height.equalTo(28)
+//            
+//            
+//            //TODO: Make a resubale method for this...center(a, b) -> cVal
+//            let frameCenter   = Int(super.view.frame.width / 2.0)
+//            let textBoxCenter = Int(password.frame.width / 2.0)
+//            let centerVal     = CGFloat(frameCenter - textBoxCenter)
+//            
+//            make.left.equalTo(loginButton.snp_left)
+//        }
+//        
+//        loginButton.snp_makeConstraints { (make) -> Void in
+//            make.top.equalTo(password.snp_bottom).offset(20)
+//            make.width.equalTo(204)
+//            make.height.equalTo(28)
+//            
+//            //TODO: Make a resubale method for this...center(a, b) -> cVal
+//            let frameCenter   = Int(super.view.frame.width / 2.0)
+//            let textBoxCenter = Int(loginButton.frame.width / 2.0)
+//            let centerVal     = CGFloat(frameCenter - textBoxCenter)
+//            
+//            make.left.equalTo(centerVal)
+//        }
+//        
+//        
+//    }
     
     
     //MARK: - IBActions
