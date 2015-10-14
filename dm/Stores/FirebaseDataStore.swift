656f24b355d0b8c1ab5cf966c7657c40d8143bee
mpc <mcampbell@elevenfiftyconsulting.com>
2015-10-14 11:55:27 -0400

    Added FirebaseDataStore

---
+++ dm/Stores/FirebaseDataStore.swift
@@ -0,0 +1,34 @@
+//
+//  FirebaseDataStore.swift
+//  dm
+//
+//  Created by mpc on 10/14/15.
+//  Copyright © 2015 mpc. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+//MARK: - Firebase setup
+var fbRef = Firebase(url: "https://dmapp.firebaseio.com")
+
+//MARK: - CRUD
+//TODO: CRUD is stubbed in.  Correct function signatures
+func create(data: String) {
+    
+}
+
+func read() -> String {
+    var ret : String = ""
+    
+    
+    return ret
+}
+
+func update() {
+    
+}
+
+func delete() {
+    
+}
