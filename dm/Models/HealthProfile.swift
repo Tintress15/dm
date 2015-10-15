f39ad4e1db8f6cf458d01d32fccdf71475e478cd
mpc <mcampbell@elevenfiftyconsulting.com>
2015-10-15 15:53:20 -0400

    Begin work on Models.

--- dm/Models/HealthProfile.swift
+++ dm/Models/HealthProfile.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+class HealthProfile {
+    var height : Float
+    var weight : Float
+    var meds   : [Meds]   //TODO: Create meds class
+    
+    
+    init() {
+        height = 0.0
+        weight = 0.0
+    }
+    
+    
+}
