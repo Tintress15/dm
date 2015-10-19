6a3e0bdf0597db65e11c3b401bee47f72c0f8714
mpc <mcampbell@elevenfiftyconsulting.com>
2015-10-18 22:31:10 -0400

    Update FirebaseDataStore

--- dm/Stores/FirebaseDataStore.swift
+++ dm/Stores/FirebaseDataStore.swift
@@ -9,26 +9,55 @@
 import Foundation
 import Firebase
 
-//MARK: - Firebase setup
-var fbRef = Firebase(url: "https://dmapp.firebaseio.com")
 
-//MARK: - CRUD
-//TODO: CRUD is stubbed in.  Correct function signatures
-func create(data: String) {
+class FirebaseDataStore {
+    //MARK: - Singleton
+    class var shared: FirebaseDataStore {
+        struct Static {
+            static let instance = FirebaseDataStore()
+        }
+        return Static.instance;
+    }
     
+    
+    //MARK: - Firebase setup
+    var fbRef = Firebase(url: "https://dmapp.firebaseio.com/")
+    
+    //MARK: - CRUD
+    //TODO: CRUD is stubbed in.  Correct function signatures
+    func create(data: String) {
+        
+    }
+    
+    func read() -> String {
+        var ret : String = ""
+        ret = "nothing"
+        
+        return ret
+        
+        
+        /*** Sample code
+        // Get a reference to our posts
+        var ref = Firebase(url:"https://docs-examples.firebaseio.com/web/saving-data/fireblog/posts")
+        // Get the data on a post that has changed
+        ref.observeEventType(.ChildChanged, withBlock: { snapshot in
+        let title = snapshot.value.objectForKey("title") as? String
+        println("The updated post title is \(title)")
+        })
+        ***/
+        
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
 }
 
-func read() -> String {
-    var ret : String = ""
-    
-    
-    return ret
-}
 
-func update() {
-    
-}
 
-func delete() {
-    
-}
+
